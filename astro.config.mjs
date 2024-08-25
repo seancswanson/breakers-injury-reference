1e75bb21ee8faee3f42473368de7cf7cc7ca9020
Sean Swanson <seancswan@gmail.com>
2024-08-24 23:43:30 -0700

    Add website URL to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://breakersinjuryreference.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
